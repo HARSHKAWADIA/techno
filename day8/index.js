b790f03792eb6a36e4ea07462ab6bf48823b42fa
Harsh Kawadia <harshkawadia000@gmail.com>
2024-08-13 12:24:12 +0530

    Day-8

---
+++ day8/index.js
@@ -0,0 +1,124 @@
+// console.log("hello world");
+
+//alert("hello world");
+
+//var,let,const
+
+// var firstterm = 20;
+
+// console.log(firstterm);
+
+//primitive types - string,number,boolean,undefined,symbol,bigint
+//reference or object type data type - array,object,data,null
+
+//var firstvar = "harsh"
+
+//console.log( firstvar)
+//console.log(typeof firstvar)
+
+// var firstvar = "i' m full stack developer"
+// console.log( firstvar)
+
+
+// var firstvar = 20
+// console.log( firstvar)
+// console.log( typeof firstvar)
+
+
+// var firstvar = null
+// console.log( firstvar)
+// console.log( typeof firstvar)
+
+// var firstvar = {
+//     firstname:"harsh",
+//     lastname:"kawadia"
+// };
+// console.log( firstvar)
+// console.log( typeof firstvar)
+
+// var date = new Date();
+// console.log(date);
+// console.log( typeof Date)
+
+//[{},{},{}]-JSON
+
+// var firstvar="hello"
+// firstvar=[20,30]
+// firstvar={
+//     firstname:"harsh",
+//     lastname:"kawadia"
+// };
+//  firstvar =true,
+
+// console.log(firstvar)
+
+// variable defien with let cannot be redeclared but cab be re assigned
+
+//variable defien with const cannot be redeclared or re assigned
+
+
+// var a = 10
+// var a =20;
+// console.log(a)
+
+
+//let a = 10
+//a =20;
+// console.log(a)
+
+// let a = 30;
+// let b = 20;
+
+// let add = a+b;
+// let sub = a-b;
+// let mul = a*b;
+// let div = a/b;
+// console.log(add);
+// console.log(sub);
+// console.log(mul);
+// console.log(div);
+
+
+// let firstname = "harsh";
+// let lastname = "kawadia";
+
+// console.log(firstname+" "+lastname)
+
+// console.log(a**b);
+// console.log(a%b);
+ 
+// console.log(5/"5");
+// console.log("5"/5);
+
+// let x = 5;
+
+// // x = x + 10
+// x+=10
+
+//  console.log(x);
+ 
+//  let str ="10";
+//  let constr = -str;
+// console.log(-str)
+// console.log(typeof  constr)
+
+// let str1 ="5";
+// let str2 =+str1;
+
+// //console.log(5+"5")
+
+// let y = 10;
+// y = y -3;
+// console.log(y);
+
+// let num4 =10;
+// let num5 ="10";
+
+// console.log(num4 == num5)
+// console.log(num4 === num5)
+
+let isloggedin = 20;
+let isadmin = 10;
+
+console.log("is the user in and an admin?",
+isloggedin && isadmin)
